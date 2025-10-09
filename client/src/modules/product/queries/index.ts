dfc25a215c1eb139cef80b3f9e3475743d1f392f
NguyenHongPhong <phongnguyenwn123@gmail.com>
2025-10-09 18:25:50 +0700

    feat(api): deleting api that not use

--- client/src/modules/product/queries/index.ts
+++ client/src/modules/product/queries/index.ts
@@ -1,24 +1,7 @@
 import { createProduct } from '@api/product';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { getProducts } from '@api/product';
-import { createCategory, getCategories } from '@api/category';
 
-
-export const useCreateCategoryMutation = () => {
-    return useMutation({
-        mutationFn: async (newCategory: any) => {
-            const { data } = await createCategory(newCategory);
-            return data;
-        }
-    });
-};
-
-export const useQueryAllCategory = () => {
-    return useQuery({
-        queryKey: ['categories'],
-        queryFn: getCategories,
-    });
-}
 
 export const useCreateProductMutation = () => {
     return useMutation({
